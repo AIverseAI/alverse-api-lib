e93b2266edf699d62d0a35c9ad58875c5a026895
PliskoBogdan <fanatamas64@gmail.com>
2023-04-20 21:24:51 +0300

    feat: remove useles code

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const Api = require('./services/ApiIntegrationService')
+const ApiIntegrationService = require('./services/ApiIntegrationService')
 
-const b = async function() {
-    const api = new Api('http://localhost:3000', { userName: '123456789@gmail.com', password: '123456789' })
-    const authService = api.getService('auth')
-    const chatBotService = api.getService('chatBot')
-    await authService.login()
-    await chatBotService.createUser()
-}
-b()
+module.exports = ApiIntegrationService;
