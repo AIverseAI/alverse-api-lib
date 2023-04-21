c9dd28cd723dfff453c1e8ec82018cb456af823f
PliskoBogdan <fanatamas64@gmail.com>
2023-04-21 19:47:51 +0300

    feat: enhance UserService

--- services/UserService.js
+++ services/UserService.js
@@ -2,17 +2,82 @@ const axios = require("../api/axiosInit");
 const store = require("../store");
 
 class UserService {
-  async findUserById(id) {
+  /**
+   * 
+   * @param {String} userId 
+   * @returns {Object} - { message, success: {true|false} }
+   */
+  async deleteUser(userId) {
     try {
-      await axios.get(`${store.baseUrl}/api/users/find-one?id=${id}`);
+      const data = await axios.delete(`${store.baseUrl}/api/users/${userId}`);
+      return data
     } catch (error) {
       console.error(error);
     }
   }
 
+  /**
+   * 
+   * @param {Number} limit 
+   * @param {Number} page 
+   * @returns {Array} [{ _id, name, role, email, telegramId }]
+   */
+  async getUsers(limit, page) {
+    try {
+      let query = `${store.baseUrl}/api/users`
+
+      if (limit) {
+        query += `?limit=${limit}`
+      }
+
+      if (page) {
+        query += `?page=${page}`
+      }
+
+      const data = await axios.get(query);
+      return data
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  /**
+   * 
+   * @param {Object} payload - { name, role, email, telegramId, password }
+   * @returns {Object} { name, role, email, telegramId, password }
+   */
+  async createNewUser(payload) {
+    try {
+      const data = await axios.post(`${store.baseUrl}/api/users`, payload);
+      return data
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  /**
+   * 
+   * @param {String} id 
+   * @returns {Object} { _id, name, role, email, telegramId }
+   */
+  async findUserById(id) {
+    try {
+      const data = await axios.get(`${store.baseUrl}/api/users/find-one?id=${id}`);
+      return data
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  /**
+   * 
+   * @param {String} id 
+   * @returns {Object} { _id, name, role, email, telegramId }
+   */
   async findUserByTgId(id) {
     try {
-      await axios.get(`${store.baseUrl}/api/users/find-one?telegramId=${id}`);
+      const data = await axios.get(`${store.baseUrl}/api/users/find-one?telegramId=${id}`);
+      return data
     } catch (error) {
       console.error(error);
     }
