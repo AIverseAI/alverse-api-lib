3e18c8e2887eb971374fdeb4ed2cb2dc913137f1
alex4jdm <alex4jdm@gmail.com>
2023-05-22 13:51:58 +0300

    feat: added update user method

--- services/UserService.js
+++ services/UserService.js
@@ -12,83 +12,106 @@ const store = require("../store");
 
 class UserService {
   /**
-   * 
-   * @param {String} userId 
+   *
+   * @param {String} userId
    * @returns {{message: String, success: Boolean}}
    */
   async deleteUser(userId) {
     try {
-      const { data } = await axios.delete(`${store.baseUrl}/api/users/${userId}`);
-      return data
+      const { data } = await axios.delete(
+        `${store.baseUrl}/api/users/${userId}`
+      );
+      return data;
     } catch (error) {
       console.error(error);
     }
   }
 
   /**
-   * 
-   * @param {Number} limit 
-   * @param {Number} page 
+   *
+   * @param {Number} limit
+   * @param {Number} page
    * @returns {userData[]}
    */
   async getUsers(limit, page) {
     try {
-      let query = `${store.baseUrl}/api/users`
+      let query = `${store.baseUrl}/api/users`;
 
       if (limit) {
-        query += `?limit=${limit}`
+        query += `?limit=${limit}`;
       }
 
       if (page) {
-        query += `?page=${page}`
+        query += `?page=${page}`;
       }
 
       const { data } = await axios.get(query);
-      return data
+      return data;
     } catch (error) {
       console.error(error);
     }
   }
 
   /**
-   * 
+   *
    * @param {Object} payload - { name, role, email, telegramId, password }
    * @returns {userData}
    */
   async createNewUser(payload) {
     try {
       const { data } = await axios.post(`${store.baseUrl}/api/users`, payload);
-      return data
+      return data;
     } catch (error) {
       console.error(error);
     }
   }
 
   /**
-   * 
-   * @param {String} id 
+   *
+   * @param {String} id
    * @returns {userData}
    */
   async findUserById(id) {
     try {
-      const { data } = await axios.get(`${store.baseUrl}/api/users/find-one?id=${id}`);
-      return data
+      const { data } = await axios.get(
+        `${store.baseUrl}/api/users/find-one?id=${id}`
+      );
+      return data;
     } catch (error) {
       console.error(error);
     }
   }
 
   /**
-   * 
-   * @param {String} id 
+   *
+   * @param {String} id
    * @returns {userData}
    */
   async findUserByTgId(id) {
     try {
-      const { data } = await axios.get(`${store.baseUrl}/api/users/find-one?telegramId=${id}`);
-      return data
+      const { data } = await axios.get(
+        `${store.baseUrl}/api/users/find-one?telegramId=${id}`
+      );
+      return data;
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  /**
+   *
+   * @param {String} telegram_id
+   * @returns {userData}
+   */
+  async updateUserByTgId(telegram_id, payload) {
+    try {
+      const { data } = await axios.patch(
+        `${store.baseUrl}/api/users/${telegram_id}`,
+        payload
+      );
+      return data;
+    } catch (error) {
+      console.log(error);
     }
   }
 }
